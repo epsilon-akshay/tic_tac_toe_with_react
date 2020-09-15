5f22d028de0c8dac618d848eeb290ea4d4223659
akshay.nambiar <akshay.nambiar@go-jek.com>
2020-09-16 03:13:45 +0530

    add winning strategy

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,10 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={() => { this.props.onClick()}}>
+      <button className="square" onClick={() => { this.props.onClick() }}>
         {this.props.value}
       </button >
     )
@@ -23,39 +43,46 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    if (this.state.xIsNext) {
-      squares[i] = 'X';
-    } else {
-      squares[i]= 'O'
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
-    
-    this.setState({ squares: squares,
-      xIsNext: !this.state.xIsNext });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={() =>this.handleClick(i)} />
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />
   }
 
   render() {
-    const status = "WELCOME TO TIC TAC AND TOE"
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div>
         <div className="status"> {status} </div>
         <div className="board-row">
+          {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-          {this.renderSquare(3)}
         </div>
         <div className="board-row">
+          {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-          {this.renderSquare(6)}
         </div>
         <div className="board-row">
+          {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
-          {this.renderSquare(9)}
         </div>
       </div>
     )
