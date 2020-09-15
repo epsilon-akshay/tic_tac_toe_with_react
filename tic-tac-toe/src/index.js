f1a920caf3c0bd9e156042f6c90149affe658825
akshay.nambiar <akshay.nambiar@go-jek.com>
2020-09-16 02:22:45 +0530

    handle state from parent component rather than child component

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
+
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" >
+      <button className="square" onClick={() => { this.props.onClick()}}>
+        {this.props.value}
       </button >
     )
   }
 }
 
 class Board extends React.Component {
-  renderSquare() {
-    return <Square />
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={() =>this.handleClick(i)} />
   }
 
   render() {
@@ -21,19 +36,19 @@ class Board extends React.Component {
       <div>
         <div className="status"> {status} </div>
         <div className="board-row">
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
         </div>
         <div className="board-row">
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
         </div>
         <div className="board-row">
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
         </div>
       </div>
     )
